b8609c00ace0a2010dcb40c69dbe69ea49edd444
Alex Muro <am3081@gmail.com>
2025-02-24 16:11:34 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { DmsSite, adminConfig, registerComponents } from "./dms/src"
+import { withAuth, useAuth } from "@availabs/ams"
+import Auth from './pages/Auth'
 import themes from "./themes"
 
+Auth.forEach(f => {
+  f.Component = f.element 
+  delete f.element
+})
 
 function App() {
   return ( 
       <DmsSite
         dmsConfig = {
           adminConfig({
-            app: 'asm',
-            type: 'nhomb'
+            app: 'dms-docs',
+            type: 'pattern-admin'
           })
-        }   
+        }
+        authWrapper={withAuth}
         themes={themes}
+        routes={[...Auth]}
       />
   )
 }
