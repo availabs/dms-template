efdd0d9109e997a2746334067df82cce19381990
Alex Muro <am3081@gmail.com>
2026-04-14 16:29:25 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { DmsSite, adminConfig  } from "./dms/packages/dms/src"
 import themes from "./themes"
 
-const API_HOST = import.meta.env.VITE_API_HOST || 'https://graph.availabs.org'
-const DAMA_HOST = import.meta.env.VITE_DAMA_HOST || 'https://graph.availabs.org'
+const API_HOST = import.meta.env.VITE_API_HOST || 'https://dmsserver.availabs.org'
+const DAMA_HOST = import.meta.env.VITE_DAMA_HOST || API_HOST
 const DMS_APP = import.meta.env.VITE_DMS_APP || 'wcdb'
 const DMS_TYPE = import.meta.env.VITE_DMS_TYPE || 'prod'
 const BASE_URL = import.meta.env.VITE_DMS_BASE_URL || '/list'
